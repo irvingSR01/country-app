09768387291681fde5bdd68837e4d54b363914d0
Irving Sanchez Roldan <irvinhirvi@gmail.com>
2024-02-15 21:26:33 -0600

    Update: Debounce created

--- src/app/shared/components/search-box/search-box.component.ts
+++ src/app/shared/components/search-box/search-box.component.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { ChangeDetectionStrategy, Component, EventEmitter, Input, OnDestroy, OnInit, Output } from '@angular/core';
+import { Subject, Subscription, debounceTime } from 'rxjs';
 
 @Component({
   selector: 'app-search-box',
@@ -15,15 +16,42 @@ import { ChangeDetectionStrategy, Component, EventEmitter, Input, OnInit, Output
   `,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class SearchBoxComponent {
+export class SearchBoxComponent implements OnInit, OnDestroy {
+
+  private debouncer: Subject<string> = new Subject<string>(); // tipo especial de observable
+  private debouncerSubscription?: Subscription;
+
   @Input()
   public placeholder: string = '';
 
   @Output()
   public onValue = new EventEmitter<string>();
 
-  emitValue( value: string ): void {
-    this.onValue.emit( value );
+  @Output()
+  public onDebounce = new EventEmitter<string>();
+
+  ngOnInit(): void {
+    this.debouncerSubscription = this.debouncer
+      .pipe(
+        debounceTime(500)
+      )
+      .subscribe(value => {
+        // console.log(`Debouncer value: ${value}`);
+        this.onDebounce.emit(value);
+      })
+  }
+
+  ngOnDestroy(): void {
+    // console.log('Destruido');
+    this.debouncerSubscription?.unsubscribe();
+  }
+
+  emitValue(value: string): void {
+    this.onValue.emit(value);
+  }
+
+  onKeyPress(searchTerm: string): void {
+    this.debouncer.next(searchTerm);
   }
 
 }
