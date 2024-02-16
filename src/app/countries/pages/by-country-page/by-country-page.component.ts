518d2fddd3eab6cde9ed6c6a3336462cebf49400
Irving Sanchez Roldan <irvinhirvi@gmail.com>
2024-02-16 12:52:02 -0600

    Update: search functionality fixed

--- src/app/countries/pages/by-country-page/by-country-page.component.ts
+++ src/app/countries/pages/by-country-page/by-country-page.component.ts
@@ -3,13 +3,15 @@ import { CountryService } from '../../services/countries.service';
 import { Country } from '../../interfaces/country';
 import { SearchBoxComponent } from '../../../shared/components/search-box/search-box.component';
 import { CountryTableComponent } from '../../components/country-table/country-table.component';
+import { LoadingSpinnerComponent } from '../../../shared/components/loading-spinner/loading-spinner.component';
 
 @Component({
   selector: 'app-by-country-page',
   standalone: true,
   imports: [
+    CountryTableComponent,
+    LoadingSpinnerComponent,
     SearchBoxComponent,
-    CountryTableComponent
   ],
   templateUrl: './by-country-page.component.html',
   styles: ``
@@ -17,11 +19,16 @@ import { CountryTableComponent } from '../../components/country-table/country-ta
 export class ByCountryPageComponent {
 
   public countries: Country[] = [];
+  public isLoading: boolean = false;
 
-  constructor(private countryService: CountryService) {}
+  constructor(private countryService: CountryService) { }
 
   searchByCountry(term: string) {
+    this.isLoading = true;
     this.countryService.searchCountry(term)
-      .subscribe(countries => this.countries = countries)
+      .subscribe(countries => {
+        this.isLoading = false;
+        return this.countries = countries
+      })
   }
 }
--- src/app/countries/pages/by-region-page/by-region-page.component.ts
+++ src/app/countries/pages/by-region-page/by-region-page.component.ts
@@ -3,23 +3,40 @@ import { Country } from '../../interfaces/country';
 import { CountryService } from '../../services/countries.service';
 import { SearchBoxComponent } from "../../../shared/components/search-box/search-box.component";
 import { CountryTableComponent } from "../../components/country-table/country-table.component";
+import { CommonModule } from '@angular/common';
+import { LoadingSpinnerComponent } from '../../../shared/components/loading-spinner/loading-spinner.component';
+
+type Region = 'Africa'|'Americas'|'Asia'|'Europe'|'Oceania';
 
 @Component({
     selector: 'app-by-region-page',
     standalone: true,
     templateUrl: './by-region-page.component.html',
     styles: ``,
-    imports: [SearchBoxComponent, CountryTableComponent]
+    imports: [
+      SearchBoxComponent,
+      CountryTableComponent,
+      CommonModule,
+      LoadingSpinnerComponent
+    ]
 })
 export class ByRegionPageComponent {
 
   public countries: Country[] = [];
+  public regions: Region[] = ['Africa', 'Americas', 'Asia', 'Europe', 'Oceania'];
+  public selectedRegion?: Region;
+  public isLoading: boolean = false;
 
   constructor(private countryService: CountryService) { }
 
-  searchByRegion(term: string) {
+  searchByRegion(term: Region) {
+    this.isLoading = true;
     this.countryService.searchRegion(term)
-      .subscribe(countries => this.countries = countries)
+    .subscribe(countries => {
+      this.isLoading = false;
+      return this.countries = countries
+    })
+    this.selectedRegion = term;
   }
 
 }
